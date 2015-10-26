327681de7c6ae9db2f32510b204f6e44c7555337
Rendxx <rendxx@gmail.com>
2015-10-26 15:57:56 -0500

    Sample edit

--- MsgBox/test/sample.js
+++ MsgBox/test/sample.js
@@ -1,8 +1,11 @@
-﻿$$.msg("This is a test message.", {
-    'top': '50px',
-    'left': '50px',
-    'padding-right': '20px'
-}, {
-    'top': '10px',
-    'right': '10px'
-}, null);
+﻿$$.msg(
+    "This is a test message.",
+    {
+        'top': '50px',
+        'left': '50px',
+        'width': '400px',
+        'padding-right': '20px'
+    }, {
+        'top': '10px',
+        'right': '10px'
+    }, null);
